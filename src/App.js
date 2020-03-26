8d917d8107fe79be16041543402cbaabe1ce5a58
Armadillo <ajknapp2015@gmail.com>
2020-03-25 18:32:09 -0700

    Set up a Reducer hook to handle the employee data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useReducer, useEffect } from 'react';
+import EmployeeContext from "./components/context/context";
+import { SET_EMPLOYEES, GET_EMPLOYEES } from './actions';
+import Axios from 'axios';
+
+const reducer = ( state, action ) => {
+
+  if(action.type === SET_EMPLOYEES) {
+    return {
+      Employees: action.payload
+    }
+  }
+
+
+  return state;
+}
 
 
 function App() {
+
+  const state = {
+    Employees: []
+  }
+
+  const [ store, dispatch ] = useReducer(reducer, state);
+
+  useEffect(()=>{
+    Axios.get("https://randomuser.me/api/?results=30&nat=us")
+    .then(({ data }) => {
+
+      dispatch({
+        type: SET_EMPLOYEES,
+        payload: data
+      })
+    })
+  },[])
+
   return (
-    <h1>Hello There</h1>
+    <React.Fragment>
+      <h1>Hello There</h1>
+      <EmployeeContext.Provider value={{store, dispatch}}>
+
+      </EmployeeContext.Provider>
+    </React.Fragment>
   );
 }
 
